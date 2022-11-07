7def0a7cc7d1c5d606c373aae1d8d9976ac5369b
DuyPham <duypham.co@gmail.com>
2022-11-07 11:53:06 +0700

    add origin reducer in store

--- admin/src/redux/store.js
+++ admin/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "./slice/loginSlice";
 import productSlice from "./slice/productSlice";
 import categorySlice from "./slice/categorySlice";
+import originSlice from "./slice/originSlice";
 
 const store = configureStore({
   reducer: {
     login: loginSlice.reducer,
     product: productSlice.reducer,
     category: categorySlice.reducer,
+    origin: originSlice.reducer,
   }
 })
 
