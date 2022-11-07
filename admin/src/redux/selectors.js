f4b78255272a1570019a5d58ebe16492da015e82
DuyPham <duypham.co@gmail.com>
2022-11-07 11:52:22 +0700

    update selector for Origin

--- admin/src/redux/selectors.js
+++ admin/src/redux/selectors.js
@@ -4,6 +4,8 @@ export const selectCategories = state => state.category.categories;
 export const selectObjectData = state => state.category.objectData;
 export const selectStatusSubmit = state => state.category.statusSubmit;
 export const selectisFetching = state => state.category.isFetching;
+export const selectProducts = state => state.product.products;
+export const selectOrigin = state => state.origin.origin;
 
 export const selectCategoriesWithoutRoot = createSelector(selectCategories, (categories) => {
   return categories.filter(item => item.parentId);
