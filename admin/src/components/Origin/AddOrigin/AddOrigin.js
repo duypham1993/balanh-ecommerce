330784528b2a3441cbcf2cee67a51dbb9a8c0ae3
DuyPham <duypham.co@gmail.com>
2022-11-07 13:31:10 +0700

    add Origin component

---
+++ admin/src/components/Origin/AddOrigin/AddOrigin.js
@@ -0,0 +1,21 @@
+import "./add-origin.scss";
+import { Grid } from "@mui/material";
+
+const AddOrigin = ({ handleOnSubmit, name, setName, formError }) => {
+  return (
+    <form onSubmit={(e) => handleOnSubmit(e)}>
+      <Grid container className="add-origin">
+        <Grid item className="add-origin__title">
+          Xuất xứ
+        </Grid>
+        <Grid item xs={9} md={3} className="add-origin__input-group">
+          <input type="text" className="input-default" name="name" vlaue={name} onChange={(e) => setName(e.target.value)} />
+          <button className="btn-default">Thêm</button>
+          <p className="add-origin__error">{formError}</p>
+        </Grid>
+      </Grid>
+    </form>
+  )
+}
+
+export default AddOrigin;
