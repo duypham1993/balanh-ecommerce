16a62bc9f0b68eff8b1f8a7e621b50c09877956e
DuyPham <duypham.co@gmail.com>
2022-11-07 13:38:31 +0700

    add origin router

--- admin/src/App.js
+++ admin/src/App.js
@@ -7,15 +7,16 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import Orders from "./pages/Orders/Orders";
-// import ProductList from "./pages/Products/ProductList/ProductList";
+import ProductList from "./pages/Products/ProductList";
 import Categories from "./pages/Categories/Categories";
 import Customer from "./pages/Customer/Customer";
 import Admins from "./pages/Admins/Admins";
-// import AddProduct from './pages/Products/AddProduct/AddProduct';
+import AddProduct from './pages/Products/AddProduct/AddProduct';
 import Box from '@mui/material/Box';
 import Sidebar from './components/Sidebar/Sidebar';
 import CreateCategory from './pages/Categories/CreateCategory/CreateCategory';
 import UpdateCategory from './pages/Categories/UpdateCategory/UpdateCategory';
+import Origin from './pages/Origin/Origin';
 
 function App() {
   const currentUser = JSON.parse(localStorage.getItem("currentUser")) || null;
@@ -36,7 +37,9 @@ function App() {
               <Route path="add-product" element={<AddProduct />} />
               <Route path="categories" element={<Categories />} />
               <Route path="categories/create" element={<CreateCategory />} />
-              <Route path="categories/:id" element={<UpdateCategory />} />
+              <Route path="categories/:id"
+                element={<UpdateCategory />} />
+              <Route path='origin' element={<Origin />} />
               <Route path="customer" element={<Customer />} />
               <Route path="admins" element={<Admins />} />
             </Routes>
