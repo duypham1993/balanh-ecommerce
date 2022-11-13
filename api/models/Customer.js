c70339e1d5f70e71196fb6063dde4ccda7cec642
DuyPham <duypham.co@gmail.com>
2022-11-13 23:22:14 +0700

    fix customer api

--- api/models/Customer.js
+++ api/models/Customer.js
@@ -1,14 +1,25 @@
+import { ObjectId } from "mongodb";
 import mongoose from "mongoose";
 
 const customerSchema = new mongoose.Schema(
   {
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
     gender: { type: String, required: true },
+    phone: { type: String, required: true },
+    dateOfBirth: { type: String },
     isActive: { type: Boolean, default: false },
-    address: { type: Object }
+    deliveryInfo: [
+      {
+        _id: { type: ObjectId, unique: true },
+        alias: { type: String },
+        name: { type: String },
+        address: { type: Object },
+        phone: { type: String },
+        other: { type: String }
+      }
+    ]
   },
   { timestamps: true }
 )
--- api/routes/customer.js
+++ api/routes/customer.js
@@ -1,52 +1,78 @@
 import express from "express";
+import CryptoJS from "crypto-js";
 import Customer from "../models/Customer";
-import { verifyToken, verifyTokenAndAuthorization } from "./verifyToken";
+import { verifyToken, verifyTokenRoleAdmin } from "./verifyToken";
 
 const router = express.Router();
 
-// GET
-router.get("/get:id", verifyToken, async (req, res) => {
-  const customer = await Customer.findById(req.params.id);
-  const { password, ...others } = customer._doc;
+// CREATE
+router.post("/create", verifyTokenRoleAdmin, async (req, res) => {
+  const newUser = new Customer({
+    name: req.body.name,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_KEY).toString(),
+    phone: req.body.phone,
+    gender: req.body.gender,
+    dateOfBirth: req.body.dateOfBirth,
+    isActive: req.body.isActive
+  });
   try {
-    res.status(200).json(others);
-  } catch (err) {
-    res.status(500).json(err);
+    const saveUser = await newUser.save();
+    res.status(201).json(saveUser);
+  } catch (error) {
+    res.status(501).json(error);
   }
 });
 
 // GET ALL 
 router.get("/", verifyToken, async (req, res) => {
-  const customer = await Customer.find({});
+  const user = await Customer.find({});
   try {
-    res.status(200).json(customer);
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  if (req.body.password) {
-    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_KEY).toString();
-  }
+router.put("/:id", verifyTokenRoleAdmin, async (req, res) => {
   try {
-    const updateCustomer = await Customer.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    res.status(200).json(updateCustomer);
+    let updateUser = {};
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_KEY).toString();
+      updateUser = await Customer.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+    } else {
+      updateUser = await Customer.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            gender: req.body.gender,
+            dateOfBirth: req.body.dateOfBirth,
+            isActive: req.body.isActive
+          }
+        },
+        { new: true }
+      );
+    }
+
+    res.status(200).json(updateUser);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // DELETE
-router.delete("/delete", verifyToken, async (req, res) => {
+router.delete("/delete/:id", verifyTokenRoleAdmin, async (req, res) => {
   try {
-    await Customer.findByIdAndDelete(req.body.id);
-    res.status(200).json("Đã xoá thành công!");
+    await Customer.findByIdAndDelete(req.params.id);
+    res.status(200).json(req.params.id);
   } catch (err) {
     res.status(500).json(err);
   }
